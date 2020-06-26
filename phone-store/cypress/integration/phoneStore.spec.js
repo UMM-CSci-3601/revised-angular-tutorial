e1299b77baf7689624d74ba14759b3e1ecf93788
K.K. Lamberty <lamberty@morris.umn.edu>
2020-06-26 18:55:08 -0500

    Include test for title links having details and adjust formatting
    This commit includes some exploration of how to get at attributes of a tag. It also includes some updates to match the general structure of 'describe' and 'it' like we had been using in Protractor. It is not completely updated, but I'm breaking for now and wanted to push these changes.

--- phone-store/cypress/integration/phoneStore.spec.js
+++ phone-store/cypress/integration/phoneStore.spec.js
@@ -1,25 +1,40 @@
 /// <reference types="cypress" />
 
-context('Viewport', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:4200/')
-  })
+describe('Phone store home page', () => {
 
-  it('cy.title() - get the title', () => {
-    // https://on.cypress.io/title
-    cy.title().should('include', 'PhoneStore')
-  })
+  context('Viewport', () => {
+    beforeEach(() => {
+      cy.viewport(400, 800)
+      cy.visit('http://localhost:4200/')
+    })
 
-  it('should have 3 products', () => {
-    cy.get('.product-entry').should('have.length', 3)
-  })
+    it('cy.title() - get the title', () => {
+      // https://on.cypress.io/title
+      cy.title().should('include', 'PhoneStore')
+    })
 
-  it('should have each product be a link', () => {
-    cy.get('.product-entry a').should('exist')
-  })
+    it('should display the name of my store, which needs to include "store"', () => {
+      cy.get('h1').contains('store', { matchCase: false})
+    })
 
-  it('should have details as part of each link', () => {
-    cy.get('product-entry a[title]').contains('details')
-  })
+    describe('product list', () => {
+      it('should have 3 products', () => {
+        cy.get('.product-entry').should('have.length', 3)
+      })
 
+      it('should have each product be a link', () => {
+        cy.get('.product-entry a').should('exist')
+      })
+
+      it('should have details as part of each link', () => {
+        cy.get('.product-entry a')
+          .should(($productEntries) => {
+            for (i=0; i<$productEntries.length; i++) {
+              console.log($productEntries.get(i).getAttribute('title'))
+              expect($productEntries.get(i).getAttribute('title')).to.contain('details')
+            }
+          })
+      })
+    })
+  })
 })
