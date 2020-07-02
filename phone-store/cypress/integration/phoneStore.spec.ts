50b6bcd0962f3f59a0eb25f44a0b4807992b4ac4
Nic McPhee <mcphee@morris.umn.edu>
2020-07-02 13:41:52 -0500

    Add two more tests using `get`s from the Cypress playground
    @KKLamberty and I played with the Cypress Playground some and found that `cy.get('.product-entry h3 a[title]')`allows us to directly access the title entries on the product entry links. This adds two tests based on that.
    
    The one problem that needed to be addressed when turned that into a test is the fact that one _could_ have product entries that simply don't have the rest of the pattern (`h3 a`) and the test would pass because it would just ignore those entries.
    
    The first test deals with this by confirming that there are three instances of the desired pattern (so every product entry has the pattern), and then checking that each title has the desired pattern.
    
    The second test avoids the complication of two checks by first extracting all the product entries, and then checking that each of them matches the sub-pattern `h3 > a[title]`, etc., etc. This is nice because the `productEntry.find('h3 > a[title]')` call returns `undefined` if the given product entry doesn't have the expected structure, which then causes the test to fail. It also avoids hard-coding `3` into the test, so it shouldn't be as fragile in the face of adding to or removing from the list of products.

--- phone-store/cypress/integration/phoneStore.spec.ts
+++ phone-store/cypress/integration/phoneStore.spec.ts
@@ -32,7 +32,7 @@ describe('Phone store home page', () => {
       })
 
       it('should display the name of my store, which needs to include "store"', () => {
-        page.topBarTitle().contains('store', { matchCase: false})
+        page.topBarTitle().contains('store', { matchCase: false })
       })
 
       it('should link to "/"', () => {
@@ -67,14 +67,57 @@ describe('Phone store home page', () => {
       })
     })
 
-    it('should have each product be a link (custom)', () =>{
+    it('should have each product be a link (custom)', () => {
       page.productListEntries().shouldEachHave('a')
     })
+
+    it('should have "details" as part of the title of each link (no page objects)', () => {
+      /*
+       * This asserts that there are three:
+       *   - product entries
+       *   - that have an `a` tag
+       *   - that contain titles
+       * Since there are only three product entries this implicitly
+       * asserts that _all_ product entries have `a` tags that
+       * contain titles.
+       */
+      cy.get('.product-entry > h3 > a[title]').should('have.length', 3);
+      /*
+       * This asserts that every
+       *  - title
+       *  - inside an `a` tag
+       *  - inside a product entry
+       * ends with the string " details". Note that this does
+       * _not_ require that, e.g., product entries _have_ these
+       * elements, just that if they do they need to match this
+       * pattern.
+       *
+       */
+      cy.get('.product-entry > h3 > a[title]').attribute('title').each((title) => {
+        expect(title).to.match(/.* details/);
+      });
+    });
+
+    /*
+     * This checks that each product entry in fact has a link with a title, and that
+     * title should end in "details".
+     *
+     * The `find` command will also return `undefined` if there's not a link with
+     * a title in the `productEntry`. This means this test will fail if any individual
+     * product entry fails to have the desired structure, which is nice. The
+     * previous test does not do that, which is why we have the `length` test
+     * there to make sure they all have the desired structure.
+     */
+    it('should have "details" as part of the title of each link (no page objects, each product-entry)', () => {
+      cy.get('.product-entry').each((productEntry) => {
+        expect(productEntry.find('h3 > a[title]').attr('title')).to.match(/.* details/);
+      });
+    });
 
     it('should have "details" as part of the title of each link (ugly) given links', () => {
       page.productListEntryLinks()
         .should(($productEntries) => {
-          for (var _i=0; _i<$productEntries.length; _i++) {
+          for (var _i = 0; _i < $productEntries.length; _i++) {
             expect($productEntries.get(_i).getAttribute('title')).to.contain('details')
           }
         })
