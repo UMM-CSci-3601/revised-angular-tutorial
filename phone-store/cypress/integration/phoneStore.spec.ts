ae7beb5e1dce606cd48a2e71e6d8b6948192ac5b
Nic McPhee <mcphee@morris.umn.edu>
2020-07-03 11:50:14 -0500

    Add a bunch of missing semi-colons
    Co-authored-by: K.K. Lamberty <lamberty@morris.umn.edu>

--- phone-store/cypress/integration/phoneStore.spec.ts
+++ phone-store/cypress/integration/phoneStore.spec.ts
@@ -1,75 +1,75 @@
 /// <reference types="cypress" />
 
-import { PhoneStoreHomePage } from "../support/phone-store-home-page.po"
+import { PhoneStoreHomePage } from "../support/phone-store-home-page.po";
 
 describe('Phone store home page', () => {
-  const page = new PhoneStoreHomePage
+  const page = new PhoneStoreHomePage;
 
   beforeEach(() => {
-    cy.viewport(400, 600)
-    page.navigate()
-  })
+    cy.viewport(400, 600);
+    page.navigate();
+  });
 
   it('should have a project title of "PhoneStore"', () => {
     // this test was not in our older Protractor tests
-    cy.title().should('be', 'PhoneStore')
-  })
+    cy.title().should('be', 'PhoneStore');
+  });
 
   describe('top bar', () => {
     it('should exist', () => {
-      page.topBar().should('exist')
-    })
+      page.topBar().should('exist');
+    });
 
     it('should display the checkout button', () => {
       page.checkoutButton()
         .should('exist')
-        .and('be.visible')
-    })
+        .and('be.visible');
+    });
 
     describe('title', () => {
       it('should exist', () => {
-        page.topBarTitle().should('exist')
-      })
+        page.topBarTitle().should('exist');
+      });
 
       it('should display the name of my store, which needs to include "store"', () => {
-        page.topBarTitle().contains('store', { matchCase: false })
-      })
+        page.topBarTitle().contains('store', { matchCase: false });
+      });
 
       it('should link to "/"', () => {
-        page.topBar().click()
-        cy.url().should('eq', page.home)
-      })
-    })
-  })
+        page.topBar().click();
+        cy.url().should('eq', page.home);
+      });
+    });
+  });
 
   describe('product list', () => {
     it('should exist', () => {
-      page.productList().should('exist')
-    })
+      page.productList().should('exist');
+    });
 
     it('should have a header', () => {
-      page.productListHeader().should('exist')
-    })
+      page.productListHeader().should('exist');
+    });
 
     it('should have three products', () => {
-      page.productListEntries().should('have.length', 3)
-    })
+      page.productListEntries().should('have.length', 3);
+    });
 
     it('should have each product be a link', () => {
       page.productListEntries().each(entry => {
-        cy.wrap(entry).should('have', 'a')
-      })
-    })
+        cy.wrap(entry).should('have', 'a');
+      });
+    });
 
     it('should have each product be a link (chai)', () => {
       page.productListEntries().each(entry => {
-        expect(entry).to.have.descendants('a')
-      })
-    })
+        expect(entry).to.have.descendants('a');
+      });
+    });
 
     it('should have each product be a link (custom)', () => {
-      page.productListEntries().shouldEachHave('a')
-    })
+      page.productListEntries().shouldEachHave('a');
+    });
 
     it('should have "details" as part of the title of each link (no page objects)', () => {
       /*
@@ -123,51 +123,50 @@ describe('Phone store home page', () => {
       page.productListEntryLinks()
         .should(($productEntries) => {
           for (var _i = 0; _i < $productEntries.length; _i++) {
-            expect($productEntries.get(_i).getAttribute('title')).to.contain('details')
+            expect($productEntries.get(_i).getAttribute('title')).to.contain('details');
           }
-        })
-    })
+        });
+    });
 
     it('should have "details" as part of the title of each link (each, chai) given links', () => {
       page.productListEntryLinks().attribute('title')
         .each(($entry) => {
-          expect($entry).to.match(/.* details$/)
-        })
-    })
+          expect($entry).to.match(/.* details$/);
+        });
+    });
 
     it('should have "details" as part of the title of each link (each, chai) given entries', () => {
       page.productListEntries()
         .each(($entry) => {
           expect($entry.find('a').attr('title')).to.match(/.* details$/);
-        })
-    })
+        });
+    });
 
     it('should have "details" as part of the title of each link (each, cypress-wrapped) given links', () => {
       page.productListEntryLinks().attribute('title')
         .each(($entry) => {
-          cy.wrap($entry).should('match', /.* details$/)
-        })
-    })
+          cy.wrap($entry).should('match', /.* details$/);
+        });
+    });
 
     it('should have "details" as part of the title of each link (each, cypress-wrapped, within) given entries', () => {
       page.productListEntries().each(($el) => {
         cy.wrap($el).within(() => {
-          cy.get('a').attribute('title').should('match', /.* details$/)
-        })
-      })
-    })
+          cy.get('a').attribute('title').should('match', /.* details$/);
+        });
+      });
+    });
 
     it('should have "details" as part of the title of each link (each, cypress-wrapped, find) given entries', () => {
       page.productListEntries().each((el) => {
-        cy.wrap(el).find('a').attribute('title').should('match', /.* details$/)
-      })
-    })
+        cy.wrap(el).find('a').attribute('title').should('match', /.* details$/);
+      });
+    });
 
     it('should have "details" as part of the title of each link (each, cypress-wrapped) given links', () => {
       page.productListEntryLinks().each((el) => {
-        cy.wrap(el).attribute('title').should('match', /.* details$/)
-      })
-    })
-
-  })
-})
+        cy.wrap(el).attribute('title').should('match', /.* details$/);
+      });
+    });
+  });
+});
